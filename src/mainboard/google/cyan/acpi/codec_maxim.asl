MrChromebox/coreboot

google/cyan: fix ACPI interrupt triggering for audio codecs
Adjust GpioInt trigger type for both LPEA and audio codec
to match Intel CHT reference code.  Fixes jack detection
under Windows.

Change-Id: Ic1dd8ece610d761791c060ece2d0aa51addf97ad
Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

src/mainboard/google/cyan/acpi/codec_maxim.asl
@@ -47,7 +47,7 @@ Scope (\_SB.PCI0.I2C2)
 					"\\_SB.PCI0.I2C2",	/* ResourceSource: I2C bus controller name */
 				)
 
-				GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullNone,,
+				GpioInt (Edge, ActiveHigh, ExclusiveAndWake, PullNone,,
 					"\\_SB.GPSE") { BOARD_JACK_MAXIM_GPIO_INDEX }
 			} )
 			Return (SBUF)
@@ -91,7 +91,7 @@ Scope (\_SB.PCI0.I2C2)
 					"\\_SB.PCI0.I2C2",	/* ResourceSource: I2C bus controller name */
 				)
 
-				GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullNone,,
+				GpioInt (Edge, ActiveHigh, ExclusiveAndWake, PullNone,,
 					"\\_SB.GPSW") { BOARD_JACK_TI_GPIO_INDEX }
 
 			} )
@@ -111,7 +111,7 @@ Scope (\_SB.PCI0.LPEA)
 	Name (GBUF, ResourceTemplate ()
 	{
 		/* Jack Detect (index 0) */
-		GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullNone,,
+		GpioInt (Edge, ActiveBoth, ExclusiveAndWake, PullNone,,
 			"\\_SB.GPSE") { BOARD_JACK_MAXIM_GPIO_INDEX }
 	})
 }

src/mainboard/google/cyan/acpi/codec_realtek.asl
@@ -39,7 +39,7 @@ Scope (\_SB.PCI0.I2C5)
 				)
 
 			 /* Jack Detect (index 0) */
-			 GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullNone,,
+			 GpioInt (Edge, ActiveHigh, ExclusiveAndWake, PullNone,,
 				  "\\_SB.GPSW") { JACK_DETECT_GPIO_INDEX }
 			} )
 			Return (SBUF)
@@ -57,7 +57,7 @@ Scope (\_SB.PCI0.LPEA)
 	Name (GBUF, ResourceTemplate ()
 	{
 		/* Jack Detect (index 0) */
-		GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullNone,,
+		GpioInt (Edge, ActiveBoth, ExclusiveAndWake, PullNone,,
 			 "\\_SB.GPSW") { JACK_DETECT_GPIO_INDEX }
 	})
 }

spectrio/vs.coreboot

src/mainboard/google/cyan/acpi/codec_maxim.asl
@@ -47,7 +47,7 @@ Scope (\_SB.PCI0.I2C2)
 					"\\_SB.PCI0.I2C2",	/* ResourceSource: I2C bus controller name */
 				)
 
-				GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullDefault,,
+				GpioInt (Edge, ActiveHigh, ExclusiveAndWake, PullDefault,,
 					"\\_SB.GPSE") { BOARD_JACK_MAXIM_GPIO_INDEX }
 			} )
 			Return (SBUF)
@@ -91,7 +91,7 @@ Scope (\_SB.PCI0.I2C2)
 					"\\_SB.PCI0.I2C2",	/* ResourceSource: I2C bus controller name */
 				)
 
-				GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullDefault,,
+				GpioInt (Edge, ActiveHigh, ExclusiveAndWake, PullDefault,,
 					"\\_SB.GPSW") { BOARD_JACK_TI_GPIO_INDEX }
 
 			} )
@@ -111,7 +111,7 @@ Scope (\_SB.PCI0.LPEA)
 	Name (GBUF, ResourceTemplate ()
 	{
 		/* Jack Detect (index 0) */
-		GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullDefault,,
+		GpioInt (Edge, ActiveBoth, ExclusiveAndWake, PullDefault,,
 			"\\_SB.GPSE") { BOARD_JACK_MAXIM_GPIO_INDEX }
 	})
 }

src/mainboard/google/cyan/acpi/codec_realtek.asl
@@ -39,7 +39,7 @@ Scope (\_SB.PCI0.I2C5)
 				)
 
 			 /* Jack Detect (index 0) */
-			 GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullDefault,,
+			 GpioInt (Edge, ActiveHigh, ExclusiveAndWake, PullDefault,,
 				  "\\_SB.GPSW") { JACK_DETECT_GPIO_INDEX }
 			} )
 			Return (SBUF)
@@ -57,7 +57,7 @@ Scope (\_SB.PCI0.LPEA)
 	Name (GBUF, ResourceTemplate ()
 	{
 		/* Jack Detect (index 0) */
-		GpioInt (Edge, ActiveLow, ExclusiveAndWake, PullDefault,,
+		GpioInt (Edge, ActiveBoth, ExclusiveAndWake, PullDefault,,
 			 "\\_SB.GPSW") { JACK_DETECT_GPIO_INDEX }
 	})
 }